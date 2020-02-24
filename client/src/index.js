dab99ca2e73753638f8b70f0b7751fa2e4775c74
AddisonBiaggi <addisonbiaggi@gmail.com>
2020-02-24 18:48:33 -0500

    removing yarn

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 
 ReactDOM.render(
