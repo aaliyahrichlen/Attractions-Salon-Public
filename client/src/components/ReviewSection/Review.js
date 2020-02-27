e456789abb2b8d892b9eb0e976e54147ff18bc0f
Gary <gary0422wu@yahoo.com>
2020-02-27 18:41:20 -0500

    first review commit

---
+++ client/src/components/ReviewSection/Review.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Review.css';
+
+const Review = () => {
+    return (
+        <footer className= "Review">
+            <h1>Reviews</h1>
+            <h3>Jackie Marsh</h3>
+            <p>Been coming here for years, my stylist is Brandi. 
+                I recommend to anyone needing a great cut/ style and extras</p>
+
+        </footer>
+    )
+};
+
+export default Review;
