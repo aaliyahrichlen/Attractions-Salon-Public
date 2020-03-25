5939d22dcd450ce0a32658e1a5f62b1421076814
aaliyahrichlen <aaliyahrichlen@ufl.edu>
2020-03-25 14:01:35 -0400

    reviews are now pulled from google

--- client/src/components/ReviewSection/Review.js
+++ client/src/components/ReviewSection/Review.js
@@ -1,45 +1,118 @@
-import React from 'react';
-import ReactDOM from "react-dom";
+import React, {Component} from 'react';
+//import ReactDOM from "react-dom";
 import './Review.css';
-import ReviewBlock from './reviewBlock';
+import  Carousel  from  'semantic-ui-carousel-react';
 
-const Review = () => {
-    return (
-        <footer className= "Review">
-            <div className= "reviewHeader">
-                Reviews
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJLbSMfFK76IgRiFeXZHbznxo&key=AIzaSyDhLHQTNwZu40NisFTOX1XpgRWEQQnG3R0";
+
+
+class Review extends React.Component { // you can't use stateless component because you need a state
+  constructor () {
+    super()
+    this.state = {}
+  }
+  componentDidMount() {
+    fetch(proxyurl + url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    }).then((value) => {
+      this.setState({val: value})
+    })
+  }
+  render () {
+    if (!this.state.val) return null
+    let reviewObj = this.state.val.result.reviews;
+    console.log(reviewObj)
+    
+    let  elements  = [
+      {
+        render:()=>{
+          return (
+            <div>
+              <img
+              className="imgProp"
+              src={reviewObj[0].profile_photo_url}
+            />
+              <div className="name">{reviewObj[0].author_name}</div>
+              <div className="content">{reviewObj[0].text}</div>
             </div>
-
-
-            <ReviewBlock 
-                block={{name: "Melissa", content: "Beautiful building that holds beautiful spirits. Brandi, you are great. Thanks for the great hair cut."}}
+            );
+        }
+      },
+      {
+        render:()=>{
+          return (
+            <div>
+              <img
+              className="imgProp"
+              src={reviewObj[1].profile_photo_url}
             />
-            
-
-            <ReviewBlock
-                block={{name:"Jackie Marsh",
-                content:"Been coming here for years, my stylist is Brandi. I recommend to anyone needing a great cut/ style and extras"
-                }}
+              <div className="name">{reviewObj[1].author_name}</div>
+              <div className="content">{reviewObj[1].text}</div>
+            </div>
+          );
+        }
+        },
+        {
+        render:()=>{
+          return (
+            <div>
+              <img
+              className="imgProp"
+              src={reviewObj[2].profile_photo_url}
             />
-            
-            
-            
-
-            {/* <div className= "reviewBlock">
-                <div className= "name">
-                    <h4>Jackie Marsh</h4>
-                </div>
-                <div className="stars">
-                    <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
-                </div>
-                <div className= "content">
-                    <p>Been coming here for years, my stylist is Brandi. 
-                        I recommend to anyone needing a great cut/ style and extras</p>
-                </div>
-            </div> */}
-
-        </footer>
+              <div className="name">{reviewObj[2].author_name}</div>
+              <div className="content">{reviewObj[2].text}</div>
+            </div>
+          );
+        }
+        },
+        {
+        render:()=>{
+          return (
+            <div>
+              <img
+              className="imgProp"
+              src={reviewObj[3].profile_photo_url}
+            />
+              <div className="name">{reviewObj[3].author_name}</div>
+              <div className="content">{reviewObj[3].text}</div>
+            </div>
+          );
+        }
+        },
+        {
+        render:()=>{
+          return (
+            <div>
+              <img
+             className="imgProp"
+              src={reviewObj[4].profile_photo_url}
+            />
+              <div className="name">{reviewObj[4].author_name}</div>
+              <div className="content">{reviewObj[4].text}</div>
+            </div>
+          );
+        }
+        },
+    ]
+    return (
+      <div className="Review">
+        <div className= "reviewHeader">
+                  Reviews
+        </div>
+        <Carousel
+          elements  =  {  elements  }
+          duration  ={3000}
+          animation  ='slide left'
+          showNextPrev  =  {false}
+          showIndicators  ={true}
+        />
+      </div>
     )
-};
+  }
+}
 
 export default Review;
