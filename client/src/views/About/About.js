1a061219459a67167b7df8877f9d31a902ef685f
Santoshkrishna Tirumala <44034516+skbt27i@users.noreply.github.com>
2020-03-28 15:36:41 -0400

    modify about page

--- client/src/views/About/About.js
+++ client/src/views/About/About.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import './About.css';
 import salonImg from './salon.jpg'
+import fire from "../Login/config/Fire";
 
-function About() {
+const About =() => {
+    const [text, setText] = useState("");
+    
+    useEffect(() => {
+        var db = fire.database();
+        var ref = db.ref("text/about");
+        ref.on("value", function(snapshot) {
+                setText(snapshot.val());
+        });
+    },[]);
+
+
     return (
         <div class="container2"> {/*I had to change this because if I just have it as container
         the picture goes beyond the container since I am using Bootstrap (Soham)*/}
             <img src={salonImg} class="spread"></img>
             <h4 >
-                At Attractions Salon, we're dedicated to providing a haircut you'll love
+                {text}
             </h4>
         </div>
 
