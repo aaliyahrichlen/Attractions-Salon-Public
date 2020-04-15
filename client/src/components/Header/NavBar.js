4045989fa0b36c22faca69194a919ddfbafb5e5c
Randolph Maynes <rmaynes@ufl.edu>
2020-04-15 16:24:02 -0400

    Mobile

--- client/src/components/Header/NavBar.js
+++ client/src/components/Header/NavBar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Image } from 'semantic-ui-react';
+import { Image, Segment } from 'semantic-ui-react';
 import { Button } from 'semantic-ui-react';
-import { Header } from 'semantic-ui-react';
+import { Header, Responsive, Dropdown } from 'semantic-ui-react';
 import './NavBar.css';
+import { AutoComplete } from 'material-ui';
 
 
 const NavBar = () => {
     return (
         <div className="layout">
-            <div className="comp">
+                <Responsive {...Responsive.onlyComputer}>
+                <div className="comp">
                 {/* Logo */}
                 <Link className="nav-logo" to="/">
                     <Image src='/images/logo.png' size='small' />
@@ -23,10 +25,29 @@ const NavBar = () => {
                     <Link className="nav-link" to='/Login'>Login</Link>
                     <Link className="nav-link" to='/Signup'>Signup</Link>
                 </div>
-                <div className="info">
-
                 </div>
-            </div>
+                </Responsive>
+                <Responsive  {...Responsive.onlyMobile}>
+                <div className="mobilebar">
+                        <Dropdown id = 'dropdown'
+                            Link
+                            icon ='huge bars'
+                        >
+                            <Dropdown.Menu>
+                                <Link className="nav-link" to='/Home'>Home</Link>
+                                <Link className="nav-link" to='/Services'>Services</Link>
+                                <Link className="nav-link" to='/About'>About Us</Link>
+                                <Link className="nav-link" to='/Appointments'>Appointments</Link>
+                                <Link className="nav-link" to='/Login'>Login</Link>
+                                <Link className="nav-link" to='/Signup'>Signup</Link>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        {/* Logo */}
+                        <Link id='logo' className="nav-logo" to="/">
+                            <Image src='/images/logo.png' size='small' />
+                        </Link>
+                    </div>
+                </Responsive>
         </div>
     )
 };
